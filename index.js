7ea0150b070656e383934f256f4319b8206ecc54
brian <kipkoechbrian0@gmail.com>
2022-08-20 15:09:38 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const findMatching = (array, name) => {
+  return array.filter((entry) => entry.toLowerCase() === name.toLowerCase());
+};
+const fuzzyMatch = (array, str) => {
+  return array.filter((name) => name.slice(0, str.length) == str);
+};
+const matchName = (array, str) => {
+  return array.filter((driver) => driver.name === str);
+};
